5e89692e1dbb24634e8334ba4bef35520c54f797
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-09-06 18:16:11 -0500

    Refactor and test another solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,9 +1,8 @@
-const removeFromArray = function (arr, ...arg) {
-  const itemsToDelete = [...arg];
-
+// This is solves it in-place
+const removeFromArray = function (arr, ...args) {
   for (let i = 0; i < arr.length; i++) {
     // Delete element if it is one of the args to delete
-    if (itemsToDelete.includes(arr[i])) {
+    if (args.includes(arr[i])) {
       arr.splice(i, 1);
       i--;
     }
@@ -11,5 +10,9 @@ const removeFromArray = function (arr, ...arg) {
   return arr;
 };
 
+// Creates a new array, with any values in args filtered out
+const removeFromArray2 = function (arr, ...args) {
+  return arr.filter(val => !args.includes(val));
+};
 // Do not edit below this line
 module.exports = removeFromArray;

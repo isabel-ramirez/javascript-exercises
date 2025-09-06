f47178f3e97993b20ee8fd48a7e0cb9bde1ed029
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-09-06 18:06:02 -0500

    Implement function to remove elements from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...arg) {
+  const itemsToDelete = [...arg];
+
+  for (let i = 0; i < arr.length; i++) {
+    // Delete element if it is one of the args to delete
+    if (itemsToDelete.includes(arr[i])) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line

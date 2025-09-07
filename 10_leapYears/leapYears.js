2304d225fd27d9f263660f318487a20bc797d124
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-09-06 19:04:41 -0500

    Implement leap year check function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+// a: divisible by 4
+// b: divisible by 100
+// c: divisible by 400
 
+// Leap Year is
+// (a && !b) || (a && b && c)
+// a && (!b || (b && c))
+const leapYears = function (year) {
+  return (
+    year % 4 === 0 &&
+    (!(year % 100 === 0) || (year % 100 === 0 && year % 400 === 0))
+  );
 };
 
 // Do not edit below this line

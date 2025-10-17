6d7664b93c879b4ae01eee546b7bad3759cd9cff
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-10-17 02:18:06 -0500

    fix: Add string handling

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function(n) {
-  if (n < 0) return 'OOPS';
+const fibonacci = function(num) {
+  let n;
+  if (typeof num !== 'number') {
+    n = parseInt(num);
+  } else {
+    n = num;
+  }
 
+  if (n < 0) return 'OOPS';
   if (n == 0) return 0;
   if (n == 1) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);

3fed3300449cdcd6a65c89b03574a62a4dd6de34
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-10-17 12:00:01 -0500

    feat: Implement palindrome checker

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+// Only numbers and lowercase letters
+const cleanString = function (string) {
+  const chars = string.trim().toLowerCase().split('');
+  const filteredChars = chars.filter(char => {
+    const charCode = char.charCodeAt(0);
+    return ((charCode >= 97 && charCode <= 122) || (charCode >= 48 && charCode <= 57));
+  });
+  return filteredChars.join('');
+}
 
+const palindromes = function (string) {
+  let str = cleanString(string);
+  let reversedStr = str.split('').reverse().join('');
+  return str === reversedStr;
 };
 
 // Do not edit below this line

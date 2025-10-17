6b3d6f09374319ce5afbc33f9a635d00bd2e24df
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-10-17 02:53:07 -0500

    feat: Implement function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  const sortedByAgeDesc = arr.sort((p1, p2) => {
+    let p1Age = (p1.yearOfDeath ? p1.yearOfDeath : 2025) - p1.yearOfBirth;
+    let p2Age = (p2.yearOfDeath ? p2.yearOfDeath : 2025) - p2.yearOfBirth;
+    return p2Age - p1Age;
+  })
+  return sortedByAgeDesc[0];
 };
 
 // Do not edit below this line

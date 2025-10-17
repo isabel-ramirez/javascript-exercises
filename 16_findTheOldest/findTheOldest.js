f6ec51c16bd4f2fcf591fe242fef0eb19b4ca3ea
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-10-17 03:01:51 -0500

    refactor: Create function for calculating age

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,9 +1,9 @@
+const getAge = function (person) {
+  let yearOfDeath = person.yearOfDeath ?? new Date().getFullYear();
+  return yearOfDeath - person.yearOfBirth;
+}
 const findTheOldest = function(arr) {
-  const sortedByAgeDesc = arr.sort((p1, p2) => {
-    let p1Age = (p1.yearOfDeath ? p1.yearOfDeath : 2025) - p1.yearOfBirth;
-    let p2Age = (p2.yearOfDeath ? p2.yearOfDeath : 2025) - p2.yearOfBirth;
-    return p2Age - p1Age;
-  })
+  const sortedByAgeDesc = arr.sort((p1, p2) => getAge(p2) - getAge(p1));
   return sortedByAgeDesc[0];
 };
 
